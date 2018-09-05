d26932e20477925769e5ae63e91df5ca31ab4920
Jerome David <programingjd@gmail.com>
2018-09-05 13:29:45 +0200

    -

---
+++ src/main/kotlin/info/jdavid/asynk/Body.kt
@@ -0,0 +1,61 @@
+package info.jdavid.asynk
+
+import info.jdavid.asynk.http.MediaType
+import kotlinx.coroutines.experimental.nio.aRead
+import kotlinx.coroutines.experimental.nio.aWrite
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousFileChannel
+import java.nio.channels.AsynchronousSocketChannel
+import java.nio.file.StandardOpenOption
+import java.util.concurrent.TimeUnit
+
+interface Body {
+
+  suspend fun writeTo(socket: AsynchronousSocketChannel, buffer: ByteBuffer)
+  suspend fun byteLength(): Long
+  fun contentType(): String
+
+  class FileBody(private val file: File, private val mediaType: String): Body {
+    override suspend fun byteLength() = file.length()
+    override fun contentType() = mediaType
+    override suspend fun writeTo(socket: AsynchronousSocketChannel, buffer: ByteBuffer) {
+      file.let {
+        val channel = AsynchronousFileChannel.open(it.toPath(), StandardOpenOption.READ)
+        var position = 0L
+        while (true) {
+          buffer.clear()
+          val read = channel.aRead(buffer, position)
+          if (read == -1) break
+          position += read
+          socket.aWrite(buffer.flip() as ByteBuffer, 5000, TimeUnit.MILLISECONDS)
+        }
+      }
+    }
+  }
+
+  class StringBody(text: String, private val mediaType: String): Body {
+    private val bytes = text.toByteArray()
+    override suspend fun byteLength() = bytes.size.toLong()
+    override fun contentType() = mediaType
+    override suspend fun writeTo(socket: AsynchronousSocketChannel, buffer: ByteBuffer) {
+      socket.aWrite(ByteBuffer.wrap(bytes), 5000, TimeUnit.MILLISECONDS)
+    }
+  }
+
+  class ByteBody(private val bytes: ByteArray, private val mediaType: String): Body {
+    override suspend fun byteLength() = bytes.size.toLong()
+    override fun contentType() = mediaType
+    override suspend fun writeTo(socket: AsynchronousSocketChannel, buffer: ByteBuffer) {
+      socket.aWrite(ByteBuffer.wrap(bytes), 5000, TimeUnit.MILLISECONDS)
+    }
+  }
+
+  companion object {
+    fun from(file: File) = from(file, MediaType.fromFile(file) ?: MediaType.OCTET_STREAM)
+    fun from(file: File, mediaType: String) = FileBody(file, mediaType)
+    fun from(text: String, mediaType: String = MediaType.TEXT) = StringBody(text, mediaType)
+    fun from(bytes: ByteArray, mediaType: String = MediaType.OCTET_STREAM) = ByteBody(bytes, mediaType)
+  }
+
+}
---
+++ src/main/kotlin/info/jdavid/asynk/Request.kt
@@ -0,0 +1,89 @@
+@file:Suppress("unused")
+
+package info.jdavid.asynk
+
+import info.jdavid.asynk.http.Headers
+import info.jdavid.asynk.http.Method
+
+class Request<T: Body> internal constructor(
+  val method: Method, val url: String, val headers: Headers, val body: T?
+)
+
+interface RequestDefinition<T: Body> {
+  fun build(): Request<T>
+}
+
+class RequestDefinitionWithBody<T: Body> internal constructor(
+  private val method: Method, private val url: String, private val headers: Headers, private val body: T?
+): RequestDefinition<T> {
+  override fun build() = Request(method, url, headers, body)
+}
+
+
+abstract class HeadersDefinition<T: HeadersDefinition<T>> internal constructor(
+  internal val headers: Headers = Headers()
+) {
+  internal abstract fun that(): T
+  fun setHeaders(headers: Headers): T {
+    this.headers.lines.clear()
+    this.headers.lines.addAll(headers.lines)
+    return that()
+  }
+  fun addHeader(name: String, value: String): T {
+    headers.add(name, value)
+    return that()
+  }
+  fun setHeader(name: String, value: String): T {
+    headers.set(name, value)
+    return that()
+  }
+  fun headerKeys() = headers.keys()
+  fun headerHas(name: String) = headers.has(name)
+  fun headerValue(name: String) = headers.value(name)
+  fun headerValues(name: String) = headers.values(name)
+}
+
+sealed class MethodDefinition(internal val method: Method)
+abstract class MethodDefinitionBodyRequired internal constructor(method: Method): MethodDefinition(method) {
+  fun url(url: String) = UrlDefinitionBodyRequired(method, url)
+}
+abstract class MethodDefinitionBodyAllowed internal constructor(method: Method): MethodDefinition(method) {
+  fun url(url: String) = UrlDefinitionBodyAllowed(method, url)
+}
+abstract class MethodDefinitionBodyForbidden internal constructor(method: Method): MethodDefinition(method) {
+  fun url(url: String) = UrlDefinitionBodyForbidden(method, url)
+}
+
+object Options: MethodDefinitionBodyForbidden(Method.OPTIONS)
+object Head: MethodDefinitionBodyForbidden(Method.HEAD)
+object Get: MethodDefinitionBodyForbidden(Method.GET)
+object Delete: MethodDefinitionBodyAllowed(Method.DELETE)
+object Post: MethodDefinitionBodyRequired(Method.POST)
+object Put: MethodDefinitionBodyRequired(Method.PUT)
+object Patch: MethodDefinitionBodyRequired(Method.PATCH)
+class Method(name: String): MethodDefinitionBodyAllowed(Method.from(name))
+
+sealed class UrlDefinition<T: HeadersDefinition<T>>(
+  internal val method: Method,
+  internal val url: String
+): HeadersDefinition<T>()
+
+class UrlDefinitionBodyRequired internal constructor(
+  method: Method, url: String
+): UrlDefinition<UrlDefinitionBodyRequired>(method, url) {
+  override fun that() = this
+  fun <T: Body>setBody(body: T) = RequestDefinitionWithBody(method, url, headers, body)
+}
+class UrlDefinitionBodyAllowed internal constructor(
+  method: Method, url: String
+): UrlDefinition<UrlDefinitionBodyAllowed>(method, url), RequestDefinition<Nothing> {
+  override fun that() = this
+  fun <T: Body>setBody(body: T) = RequestDefinitionWithBody(method, url, headers, body)
+  override fun build() = Request(method, url, headers, null)
+}
+class UrlDefinitionBodyForbidden internal constructor(
+  method: Method, url: String
+): UrlDefinition<UrlDefinitionBodyForbidden>(method, url), RequestDefinition<Nothing> {
+  override fun that() = this
+  override fun build() = Request(method, url, headers, null)
+}
